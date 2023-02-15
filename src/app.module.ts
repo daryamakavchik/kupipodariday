d6798ebebd1b5f1bf60d0451f173d572e1e472b9
daryamakavchik <darjamakavchik@gmail.com>
2023-02-15 15:39:53 -0800

    feat: add config and factory for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Offer } from './offers/entities/offer.entity';
-import { User } from './users/entities/user.entity';
-import { Wish } from './wishes/entities/wish.entity';
-import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from './config/database.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'student',
-      password: 'student',
-      database: 'kupipodariday',
-      entities: [User, Offer, Wish, Wishlist],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({...configService.get('database')})
+    }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
+    AuthModule,
   ],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/config/database.config.ts
@@ -0,0 +1,20 @@
+import { registerAs } from '@nestjs/config';
+import { User } from '../users/entities/user.entity';
+import { Wish } from '../wishes/entities/wish.entity';
+import { Wishlist } from '../wishlists/entities/wishlist.entity';
+import { Offer } from '../offers/entities/offer.entity';
+
+require('dotenv').config();
+
+export default registerAs('database', () => {
+  return {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [User, Wish, Wishlist, Offer],
+    synchronize: true,
+  };
+});
