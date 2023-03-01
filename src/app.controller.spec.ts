bdb5c88f0874bfb31636bb826859b6ad0b94029a
daryamakavchik <darjamakavchik@gmail.com>
2023-02-28 20:01:54 -0800

    fix: formatting

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,16 +1,14 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
-describe('AppController', () => {
-  let appController: AppController;
-
+describe("AppController", () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    app.get<AppController>(AppController);
   });
 });
